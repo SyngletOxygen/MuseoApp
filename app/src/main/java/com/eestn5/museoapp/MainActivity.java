559efa419bf79cb5cdd596085d8697e68f65a361
SyngletOxygen <93107888+SyngletOxygen@users.noreply.github.com>
2022-09-14 19:30:48 -0300

    Update MainActivity.java

--- app/src/main/java/com/eestn5/museoapp/MainActivity.java
+++ app/src/main/java/com/eestn5/museoapp/MainActivity.java
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
-
+   
 
 public class MainActivity extends AppCompatActivity {
     public static final String SCORE = "com.example.holamundo.SCORE";
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
- 
+
         setContentView(R.layout.activity_main);
 
     }
