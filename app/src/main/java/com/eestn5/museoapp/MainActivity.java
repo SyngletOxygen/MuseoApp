74182601f030d33f20968e66345c7cff8d928c7a
SyngletOxygen <93107888+SyngletOxygen@users.noreply.github.com>
2022-09-14 19:17:07 -0300

    Initial commit

---
+++ app/src/main/java/com/eestn5/museoapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.eestn5.museoapp;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+    public static final String SCORE = "com.example.holamundo.SCORE";
+
+    private boolean LockTimer = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
