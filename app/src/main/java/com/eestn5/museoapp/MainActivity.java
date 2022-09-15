2f9699d05125464ce1429f7ef99a53eee9153a8c
SyngletOxygen <93107888+SyngletOxygen@users.noreply.github.com>
2022-09-15 18:08:24 -0300

    Changed UI

--- app/src/main/java/com/eestn5/museoapp/MainActivity.java
+++ app/src/main/java/com/eestn5/museoapp/MainActivity.java
@@ -5,7 +5,11 @@ package com.eestn5.museoapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,8 +21,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menuprincipal);
+       // if (!is there any handicap defined){
+        //   run through first time setup
+        //
+        // }
+    }
+
+    public void OnClick(View button){
+        Intent intent = new Intent(this, SeleccionDeDiscapacidadActivity.class);
+        //intent.putExtra(  );
+        startActivity(intent);
+
 
     }
+
+
+    //EXAMPLE FOR SOUND
+    /* WebView mauro = this.findViewById(R.id.Maurotest);
+        mauro.getSettings().setJavaScriptEnabled(true);
+        mauro.getSettings().setLoadWithOverviewMode(true);
+        mauro.getSettings().setUseWideViewPort(true);
+        mauro.getSettings().setPluginState(WebSettings.PluginState.ON);
+        mauro.loadUrl("https://www.google.com");
+
+        */
+
+
 
 }
---
+++ app/src/main/java/com/eestn5/museoapp/SeleccionDeDiscapacidadActivity.java
@@ -0,0 +1,17 @@
+package com.eestn5.museoapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+    public class SeleccionDeDiscapacidadActivity  extends AppCompatActivity {
+
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+
+            setContentView(R.layout.activity_seleccion_discapacidad);
+
+        }
+
+}
